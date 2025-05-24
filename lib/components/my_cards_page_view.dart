21b1f08aa1e2dbc99fdfc0f166babd95b26cc290
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-05-24 21:57:50 +0300

    use the expandable page view to generate my cards

--- lib/components/my_cards_page_view.dart
+++ lib/components/my_cards_page_view.dart
@@ -1,3 +1,4 @@
+import 'package:expandable_page_view/expandable_page_view.dart';
 import 'package:flutter/material.dart';
 import 'package:ui_app/components/my_card.dart';
 
@@ -6,11 +7,12 @@ class MyCardsPageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 3,
-        itemBuilder: (context, index) {
-          return const MyCard();
-        });
+    return ExpandablePageView(
+      scrollDirection: Axis.horizontal,
+      children: List.generate(
+        3,
+        (index) => MyCard(),
+      ),
+    );
   }
 }
